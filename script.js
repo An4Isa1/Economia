489114a10a3f565731b3c67f8550f638726e94bc
An4Isa1 <isa.gordillo.molina@gmail.com>
2025-07-16 20:56:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const materiasPorSemestre = [
   [
-    { nombre: "Economía I(Introducción a la Economía)", requisitos: [] },
+    { nombre: "Economía I (Introducción a la Economía)", requisitos: [] },
     { nombre: "Historia Económica General", requisitos: [] },
     { nombre: "Matemáticas I", requisitos: [] },
     { nombre: "Expresión Oral y Esctrita", requisitos: [] },
@@ -9,19 +9,68 @@ const materiasPorSemestre = [
     { nombre: "Principios Constitucionales", requisitos: [] }
   ],
   [
-    { nombre: "Microeconomía I", requisitos: ["Fundamentos de Economía"] },
-    { nombre: "Macroeconomía I", requisitos: ["Fundamentos de Economía"] },
+    { nombre: "Microeconomía I", requisitos: ["Economía I (Introducción a la Economía)"] },
+    { nombre: "Historia Económica Colombiana", requisitos: ["Historia Económica General"] },
     { nombre: "Matemáticas II", requisitos: ["Matemáticas I"] },
-    { nombre: "Contabilidad Financiera", requisitos: ["Fundamentos de Contabilidad"] },
-    { nombre: "Estadística I", requisitos: ["Matemáticas I"] }
+    { nombre: "Estadística I", requisitos: [] },
+    { nombre: "Algebra lineal", requisitos: [] }
+    { nombre: "Humanidades I", requisitos: [] }
+    { nombre: "Extensión Cultural y Deportiva", requisitos: [] }
   ],
   [
     { nombre: "Microeconomía II", requisitos: ["Microeconomía I"] },
-    { nombre: "Macroeconomía II", requisitos: ["Macroeconomía I"] },
+    { nombre: "Macroeconomía I", requisitos: ["Microeconomía I"] },
     { nombre: "Matemáticas III", requisitos: ["Matemáticas II"] },
     { nombre: "Estadística II", requisitos: ["Estadística I"] },
-    { nombre: "Historia Económica General", requisitos: [] }
+    { nombre: "Medición Económica", requisitos: ["Microeconomía I"] }
   ]
+  [
+    { nombre: "Microeconomía III", requisitos: ["Microeconomía II"] },
+    { nombre: "Macroeconomía II", requisitos: ["Macroeconomía I"] },
+    { nombre: "Economía Matemática", requisitos: ["Matemáticas III"] },
+    { nombre: "Estadística III", requisitos: ["Estadística I"] },
+    { nombre: "Contabilidad General", requisitos: [] }
+  ]
+  [
+    { nombre: "Macroeconomía III", requisitos: ["Microeconomía III", "Macroeconomía II","Economía Matemática"] },
+    { nombre: "Doctrinas Económicas I", requisitos: [] },
+    { nombre: "Econometría I", requisitos: [] },
+    { nombre: "Énfasis I", requisitos: [] },
+    { nombre: "Matemáticas Financieras", requisitos: ["Estadística III"] }
+    { nombre: "Contabilidad de Costos", requisitos: ["Contabilidad General"] }
+  ]
+  [
+    { nombre: "Crecimiento Económico", requisitos: ["Macroeconomía III"] },
+    { nombre: "Doctrinas Económicas II", requisitos: ["Doctrinas Económicas I"] },
+    { nombre: "Econometría II", requisitos: [] },
+    { nombre: "Énfasis II", requisitos: [] },
+    { nombre: "Teoría de la Decisión", requisitos: [] }                                               
+    { nombre: "Análisis Financiero", requisitos: ["Contabilidad de Costos"] }
+  ]
+  [
+    { nombre: "Teoría y Política Fiscal", requisitos: ["Crecimiento Económico"] },
+    { nombre: "Desarrollo Económico", requisitos: [] },
+    { nombre: "Teoría y Política Monetaria y Cambiaria", requisitos: [] },
+    { nombre: "Énfasis III", requisitos: [] },
+    { nombre: "Formulación y Evaluación de Proyectos", requisitos: [] }                                               
+    { nombre: "Humanidades II", requisitos: [] }
+  ]
+  [
+    { nombre: "Electiva I en lo Público", requisitos: [] },
+    { nombre: "Electiva I en lo Económico", requisitos: [] },
+    { nombre: "Economía Internacional", requisitos: [] },
+    { nombre: "Énfasis IV", requisitos: [] },
+    { nombre: "Evaluación Económica y Social de Proyectos", requisitos: [] }
+    { nombre: "Etica Profesional", requisitos: [] },
+  ]
+  [
+    { nombre: "Electiva II en lo Público", requisitos: [] },
+    { nombre: "Electiva II en lo Económico", requisitos: [] },
+    { nombre: "Electiva en lo Internacional", requisitos: [] },
+    { nombre: "Electiva de Énfasis", requisitos: ["Estadística I"] },
+    { nombre: "Seminario de Grado", requisitos: [] }
+  ]
+
 ];
 
 const aprobadas = new Set();
